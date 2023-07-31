589ce23d4e30c260d0d720c33c2c08eaa26ff108
jamalull <jamaluldazzled@gmail.com>
2023-07-31 22:12:57 +0700

    Update for fix some layout design with grid in responsive

--- frontend/src/components/VideoList.jsx
+++ frontend/src/components/VideoList.jsx
@@ -6,7 +6,7 @@ function VideoList() {
   return (
     <>
     {/* <div className="flex flex-wrap gap-4"> */}
-    <div className="grid grid-cols-3 lg:grid-cols-6 lg:gap-4">
+    <div className="grid max-[560px]:grid-cols-1 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 lg:gap-4 pr-2">
       {console.log(dataVideoList)}
       {dataVideoList.map((video)=> {
         return (
