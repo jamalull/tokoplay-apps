28d092760028a72af55a3a835cda1066cde104ad
jamalull <jamaluldazzled@gmail.com>
2023-08-01 00:23:25 +0700

    add Channel Pages

---
+++ frontend/src/pages/Channel.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DetailVideo from "../components/DetailVideo"
+
+function Channel() {
+  return (
+    <>
+      <DetailVideo/>
+    </>
+  )
+}
+
+export default Channel
