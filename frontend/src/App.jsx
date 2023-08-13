27753f894439bd736ab5bad46068c5e012a6eff0
jamalull <jamaluldazzled@gmail.com>
2023-08-13 23:30:02 +0700

    add settings router and global context state for the component of pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home" 
 import Channel from "./pages/Channel"
+import { GlobalProvider } from "./context/GlobalContext"
 
 function App() {
 
   return (
     <>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="channel" element={<Channel/>}/>
-      </Routes>
+      <GlobalProvider>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="channel/:videoId" element={<Channel/>}/>
+        </Routes>
+      </GlobalProvider>
     </BrowserRouter>
     </> 
   )
